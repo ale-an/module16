aa884afc82deea9b093ea5fb634338fc942872cf
alena <vorozhtsova_alena@mail.ru>
2023-10-17 22:58:21 +0500

    Задание 16.4.3

--- Module16.Tests.xUnit/CalculatorTests.cs
+++ Module16.Tests.xUnit/CalculatorTests.cs
@@ -26,5 +26,21 @@ namespace Modul16.Test.xUnit
             var calculator = new Calculator();
             Assert.Throws<DivideByZeroException>((() => calculator.Division(100, 0)));
         }
+
+        [Fact]
+        public void AdditionalMustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            int result = calculator.Addition(50, 10, 34);
+            Assert.Equal(94, result);
+        }
+
+        [Fact]
+        public void MultiplicationMustReturnCorrectValue()
+        {
+            var calculator = new Calculator();
+            int result = calculator.Multiplication(3, 2, 20);
+            Assert.Equal(120, result);
+        }
     }
 }
--- Module16/Calculator.cs
+++ Module16/Calculator.cs
@@ -6,9 +6,20 @@
         {
             return a - b;
         }
+
         public int Division(int a, int b)
         {
             return a / b;
+        }
+
+        public int Addition(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+
+        public int Multiplication(int a, int b, int c)
+        {
+            return a * b * c;
         }
     }
 }
